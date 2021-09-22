a89c2b894e6faa6cd6804b782a5372b160b08e2b
Andrés Collares <andres.collares@fing.edu.uy>
2021-09-22 16:21:38 -0300

    test

--- crawler-praw.py
+++ crawler-praw.py
@@ -1,14 +1,30 @@
 import praw
 
-reddit = praw.Reddit(
-    client_id='PA80r16sX4tHy6rnYkpdyw',
-    client_secret='Tsw4ky4M2gp3l0pEbD61a2q0fOTwZQ',
-    user_agent = "crawler-ar by moy--",
-    # username = "",
-    # password = "",
-)
+reddit = praw.Reddit( client_id='PA80r16sX4tHy6rnYkpdyw', client_secret='Tsw4ky4M2gp3l0pEbD61a2q0fOTwZQ', user_agent = "crawler-ar by moy--",)
 
-# print(reddit.read_only)
+subreddit = reddit.subreddit("uruguay") # <- conseguir un subreddit
 
-for submission in reddit.subreddit("uruguay").hot(limit=10):
-    print(submission.title)
+subreddit.display_name
+
+subreddit.title
+
+for submission in subreddit.hot(limit=10): # <- recorrer los posts
+    submission.author # <- el autor
+    submission.title # <- el título
+    submission.score
+
+    # submission.comment_sort = "top" <- permite ordenar
+    submission.comments.list()
+
+redditor = reddit.redditor("moy--") # <- como conseguir un usuario
+redditor.link_karma
+redditor.comment_karma
+redditor.name
+redditor.comments
+
+redditor.subreddits # <- subreddits en los que participa
+
+
+
+
+
