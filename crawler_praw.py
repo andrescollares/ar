47ea477f266141bdc605455c55066251bce0cda0
agustingazzano <agustingazzano107@gmail.com>
2021-09-25 10:14:14 -0300

    aaa

--- crawler_praw.py
+++ crawler_praw.py
@@ -8,7 +8,7 @@ def get_data(limit = 10, save_every=10):
         client_id='BDKPcze-owFsIKSfxXXeJA',
         client_secret='Hj3Y2S1EhlqR3My35hameO0_kRMuxw',
         user_agent = "un cosito by ar-crawler")
-    df = pd.DataFrame(columns=['commenter', 'responds_to'])
+    df = pd.DataFrame(columns=['commenter', 'responds_to', 'is_post'])
     print('asd')
     index_submi = 0
     comment_count = 0
@@ -18,22 +18,22 @@ def get_data(limit = 10, save_every=10):
         for comment in submission_comments:
             # respuestas a post tienen prefijo t3_
             # respuestas a otros usuarios tienen prefijo t1_
-            if (comment.parent_id.startswith('t3')):
+            is_post = comment.parent_id.startswith('t3')
+            if (is_post):
                 responds_to = submission.author
             else:
                 parent_comment_id = comment.parent_id[3:]
                 responds_to = next((x.author for x in submission_comments if x.id == parent_comment_id), None)
-            # print(f"{comment.author} -> {responds_to}")
             # Usuarios eliminados dan None, filtraremos estos del dataset
             if comment.author != None and responds_to != None:
                 # todo: crear el dataframe despues, par amejor performance, antes guardar en aray o dict
-                df.loc[comment_count] = [comment.author, responds_to]
+                df.loc[comment_count] = [comment.author, responds_to, is_post]
                 comment_count += 1
         index_submi += 1
         print(f"Posts: {index_submi}, comentarios totales: {comment_count}")
     # guardar en CSV cada cierto tiempo (default = 10 posts)
     if index_submi % save_every == 0:
-        csv_string = df.to_csv(index=False, header=False)
+        csv_string = df.to_csv(index=False, header=True)
         with open("data.csv", "a") as csv:
             csv.write(csv_string)
         # reinicializo el dataset
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import csv
 import sys
+import pandas as pd
 
 from crawler_praw import get_data
 from graph_utils import save_graphml
@@ -13,23 +14,25 @@ try:
     f = open('data.csv', 'r')
 except FileNotFoundError:
     print("Obteniendo datos...")
-    get_data(limit=50)
+    get_data(limit=10)
 except Exception as err:
     print(f"Unexpected error opening data.csv is",repr(err))
     sys.exit(1)
 else:
     with f:
-        reader = csv.reader(f)
-        respuestas = {}
-        for row in reader:
-            print(row)
-            commenter, responds_to = row
-            if (commenter in respuestas):
-                respuestas[commenter].append(responds_to)
-            else:
-                respuestas[commenter] = [responds_to]
-            if (responds_to not in respuestas):
-                respuestas[responds_to] = []
-    save_graphml(respuestas, datetime.now().strftime("%m-%d-%Y-%H:%M"))
+        df = pd.read_csv('data.csv')
+        print(df)
+    #     reader = csv.reader(f)
+    #     respuestas = {}
+    #     for row in reader:
+    #         print(row)
+    #         commenter, responds_to = row
+    #         if (commenter in respuestas):
+    #             respuestas[commenter].append(responds_to)
+    #         else:
+    #             respuestas[commenter] = [responds_to]
+    #         if (responds_to not in respuestas):
+    #             respuestas[responds_to] = []
+    # save_graphml(respuestas, datetime.now().strftime("%m-%d-%Y-%H:%M"))
 
         
